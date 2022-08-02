7d5e0c1d27e60bffd3cabd24e6f0411dfe87049e
PiyushWadhawan <66672056+PiyushWadhawan@users.noreply.github.com>
2022-08-03 01:19:27 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Header from "./component/Header"
+import FeedbackList from "./component/FeedbackList"
+import FeedbackStats from "./component/FeedbackStats"
+import FeedbackForm from "./component/FeedbackForm"
+import AboutPage from "./pages/AboutPage"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AboutIconLink from "./component/AboutIconLink"
+import {FeedbackProvider} from "./context/FeedbackContext"
+
+
+function App()
+{
+
+    return (
+    <FeedbackProvider>
+        <Router>
+            <Header />
+            <div className="container">
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm />
+                            <FeedbackStats />
+                            <FeedbackList />
+                            <AboutIconLink />
+                        </>
+                    }>
+                    </Route>
+
+                    <Route path='/about' element={<AboutPage />} />
+
+                </Routes>   
+            </div>
+        </Router>
+    </FeedbackProvider>
+    )
+}
+
+export default App
